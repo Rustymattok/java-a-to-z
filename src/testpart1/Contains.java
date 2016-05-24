50a0701ec863569774b25fd044fed090cdc42839
Vladimir Makarov <mrArxi@gmail.com>
2016-05-24 23:25:31 +0300

    added test for part 1

---
+++ src/testpart1/Contains.java
@@ -0,0 +1,38 @@
+package testpart1;
+
+public class Contains
+{
+    public static void main(String[] args)
+    {
+        String first = "пример кода";
+        String second = "прим";
+        boolean flag = false;
+        char [] a = first.toCharArray();
+        char [] b = second.toCharArray();
+
+        for (int i = 0; i < a.length ; i++)
+        {
+            if (a[i] == b[0]){
+                if (compare(i,a,b)){
+                    flag = compare(i,a,b);
+                    break;
+                }
+            }
+        }
+        System.out.println(flag);
+
+    }
+    public static boolean compare(int k,char[] a,char[] b){
+        boolean flag = false;
+        for (int i = 0; i < b.length; i++)
+        {
+            if((a[i+k] == b[i]) && b.length <= a.length){
+                flag = true;
+            }else {
+                flag = false;
+                break;
+            }
+        }
+        return flag;
+    }
+}
