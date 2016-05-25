7d8e19cb1c876ea36e1118b5f109e1f0afc582ed
Vladimir Makarov <mrArxi@gmail.com>
2016-05-26 00:54:28 +0300

    update(delet static method)

--- src/testpart1/Contains.java
+++ src/testpart1/Contains.java
@@ -6,23 +6,23 @@ public class Contains
     {
         String first = "пример кода";
         String second = "прим";
+        Contains contains = new Contains();
         boolean flag = false;
         char [] a = first.toCharArray();
         char [] b = second.toCharArray();
-
         for (int i = 0; i < a.length ; i++)
         {
             if (a[i] == b[0]){
-                if (compare(i,a,b)){
-                    flag = compare(i,a,b);
+                if ( contains.compare(i, a, b)){
+                    flag = contains.compare(i,a,b);
                     break;
                 }
             }
         }
         System.out.println(flag);
-
     }
-    public static boolean compare(int k,char[] a,char[] b){
+
+    public boolean compare(int k,char[] a,char[] b){
         boolean flag = false;
         for (int i = 0; i < b.length; i++)
         {
