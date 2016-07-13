f87b3c2acda7b84ca93e6c40244d903823e0e630
Vladimir Makarov <mrArxi@gmail.com>
2016-07-14 01:38:56 +0300

    edited

--- src/lesson3/src/main/java/evenodd/Number.java
+++ src/lesson3/src/main/java/evenodd/Number.java
@@ -1,63 +1,37 @@
 package evenodd;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-
-public class Number implements Methods {
-
+/**
+ * This class describe number's parametres.
+ */
+public class Number {
+    /**
+     * @param Integer number - value.
+     * @param Discribe discribe - the enumeration of two position.
+     */
     private Integer number;
-
+    private Discribe discribe;
+    /**
+     * Constructor - initialization of parameters using console.
+     */
     public Number() {
         boolean flag = false;
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        while (!flag){
-        try {
-            System.out.println("Введите число: ");
-            number = Integer.valueOf(bufferedReader.readLine());
-            flag = true;
-        } catch (Exception e) {
-            System.out.println("Не корректный ввод числа");
-            flag = false;
-          }
+        while (!flag) {
+            try {
+                System.out.println("Введите число: ");
+                number = Integer.valueOf(bufferedReader.readLine());
+                flag = true;
+            } catch (Exception e) {
+                System.out.println("Не корректный ввод числа");
+                flag = false;
+            }
         }
     }
 
     public Number(Integer number) {
         this.number = number;
-    }
-
-    public void evenOdd(){
-        if (number%2 == 0 ){
-            System.out.println("Четное число");
-        }else
-        System.out.println("Нечетное число");
-    }
-
-    //test method
-    public boolean even() {
-        boolean flag = false;
-        if (number%2 == 0 ){
-           flag = true;
-            System.out.println("Четное число");
-        }else {
-            flag = false;
-            System.out.println("Нечетное число");
-        }
-        return flag;
-    }
-
-   //test method
-    public boolean odd() {
-        boolean flag = false;
-        if (number%2 != 0 ){
-            flag = true;
-            System.out.println("Нечетное число");
-        }else {
-            flag = false;
-            System.out.println("Четное число");
-        }
-        return flag;
     }
 
     public Integer getNumber() {
@@ -66,5 +40,13 @@ public class Number implements Methods {
 
     public void setNumber(Integer number) {
         this.number = number;
+    }
+
+    public Discribe getDiscribe() {
+        return discribe;
+    }
+
+    public void setDiscribe(Discribe discribe) {
+        this.discribe = discribe;
     }
 }
--- src/lesson3/src/main/java/evenodd/StartTest.java
+++ src/lesson3/src/main/java/evenodd/StartTest.java
@@ -1,8 +1,13 @@
 package evenodd;
 
+/**
+ * Class for init application.
+ */
 public class StartTest {
     public static void main(String[] args) {
-        Number number = new Number();
-        number.evenOdd();
+         Number number = new Number();
+         Logic logic = new Logic(number);
+         logic.evenOdd();
+         logic.show();
     }
 }
