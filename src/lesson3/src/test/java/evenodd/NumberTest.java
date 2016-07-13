1efe609cae5fdbbfa718fb2a16b08c3935decd32
Vladimir Makarov <mrArxi@gmail.com>
2016-07-14 01:40:48 +0300

    edited

--- src/lesson3/src/test/java/evenodd/NumberTest.java
+++ src/lesson3/src/test/java/evenodd/NumberTest.java
@@ -1,26 +1,36 @@
 package evenodd;
 
-
 import org.junit.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 
+/**
+ * Class for tests.
+ */
 public class NumberTest {
-
+    /**
+     * @param logic - initiated parameter of Logic.
+     */
+    private final Logic logic = new Logic(new Number(2));
+    /**
+     * whenShouldCheckNumberEven() - this method check for even.
+     */
     @Test
     public void whenShouldCheckNumberEven(){
         boolean result = false;
-        Number number = new Number(2);
-        result = number.even();
-        assertThat(result,is(true));
+        result = logic.even();
+        System.out.println("Четное " + result);
+        assertThat(result, is(true));//todo эта строка в этом случае не нужна?
     }
-
+    /**
+     * whenShouldCheckNumberOdd() - this method check for odd.
+     */
     @Test
     public void whenShouldCheckNumberOdd(){
         boolean result = false;
-        Number number = new Number(3);
-        result = number.odd();
-        assertThat(result,is(true));
+        result = logic.odd();
+        System.out.println("Не Четное " + result);
+        assertThat(result,is(true));//todo эта строка в этом случае не нужна?
     }
 }
