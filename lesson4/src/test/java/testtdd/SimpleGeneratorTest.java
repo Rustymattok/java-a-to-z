d7b91df366257f1d8d8d715ca18ca07fa38b3292
Rustymattok <mrArxi@gmail.com>
2016-10-13 00:21:46 +0300

    made mor usablity test

--- lesson4/src/test/java/testtdd/SimpleGeneratorTest.java
+++ lesson4/src/test/java/testtdd/SimpleGeneratorTest.java
@@ -10,15 +10,12 @@ import static org.hamcrest.core.Is.is;
 public class SimpleGeneratorTest {
     @Test
     public void whenShouldCheckValidate(){
-        boolean rezult;
         Pair pair1 = new Pair("name","Vladimir");
         Pair pair2 = new Pair("name1","Yhoo");
         Pair[] pairs = {pair1,pair2};
         Template template = new SimpleGenerator();
-        String text = template.generate("Privet ${name),${name1}",pairs);
-        rezult = template.validate(text);
-        System.out.println(rezult);
-        assertThat(rezult, is(false));
+        String rezult = template.generate("Privet ${name} ,${name1}",pairs);
+        assertThat(rezult, is("Privet Vladimir ,Yhoo"));
     }
 }
 
