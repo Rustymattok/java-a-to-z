0fe8bbd53eeca3f82bf43d02dc6170b25e501e92
Vladimir Makarov <mrArxi@gmail.com>
2016-11-02 01:39:22 +0300

    updated

--- lesson5/src/main/java/part3list/MyLinkedList.java
+++ lesson5/src/main/java/part3list/MyLinkedList.java
@@ -1,7 +1,111 @@
 package part3list;
 
+import java.util.Iterator;
+import java.util.NoSuchElementException;
 /**
- * Created by Владимир on 02.11.2016.
+ * Class wich describerealisation of LinkedList.
+ * @param <T>
  */
-public class MyLinkedList {
+public class MyLinkedList <T> implements ContainerArray<T> {
+    /**
+     * Node befor and after.
+     * @param size - size of list.
+     */
+    MyNode<T> first;
+    MyNode<T> last;
+    int size;
+    /**
+     * This method for add element in our list.
+     * @param object
+     */
+    public void add(T object) {
+        MyNode<T> next = last;
+        MyNode<T> newMode = new MyNode<T>(object,next,null);
+        this.last = newMode;
+        if(next == null) {
+            first = newMode;
+        }else{
+            next.next = newMode;
+        }
+        size++;
+    }
+    /**
+     * This method return node by index.
+     * @param index
+     * @return node.
+     */
+    MyNode<T> node(int index) {
+        MyNode<T> x = first;
+        if ( size != 0 && index < size){
+            for (int i = 0; i < index; i++) {
+                x = x.next;
+            }
+        } else {
+            throw new NoSuchElementException();
+        }
+        return x;
+    }
+    /**
+     * This element return element.
+     * @param index
+     * @return T object.
+     */
+    public T getArr(int index) {
+        return  node(index).getObject();
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public Iterator<T> iterator() {
+        return  new MyIterator<T>();
+    }
+    /**
+     * Inner class for describe Node.
+     * @param <T>
+     */
+    private class MyNode<T> {
+        T object;
+        MyNode<T> next;
+        MyNode<T> prev;
+
+        public MyNode(T object, MyNode<T> next, MyNode<T> prev) {
+            this.object = object;
+            this.next = next;
+            this.prev = prev;
+        }
+
+        public T getObject() {
+            return object;
+        }
+    }
+    /**
+     * Iterator for this class.
+     * @return
+     */
+    private class MyIterator <T> implements Iterator <T>{
+        private int indexP = 0;
+
+        public boolean hasNext() {
+            boolean flag = false;
+            if(this.indexP != size){
+                flag = true;
+            }
+            return flag;
+        }
+
+        public T next() {
+            if (this.hasNext()) {
+                return (T) getArr(indexP++);
+            } else {
+                throw new NoSuchElementException();
+            }
+        }
+
+        public void remove() {
+
+        }
+    }
 }
+
--- lesson5/src/test/java/part3list/MyLinkedListTest.java
+++ lesson5/src/test/java/part3list/MyLinkedListTest.java
@@ -1,10 +1,42 @@
 package part3list;
 
-import static org.junit.Assert.*;
+import org.junit.Test;
 
-/**
- * Created by Владимир on 02.11.2016.
- */
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
 public class MyLinkedListTest {
-
+    private MyLinkedList<String> list;
+    /**
+     * This method allow to check method add.
+     * If added position 5 and equels with result.
+     * @throws Exception
+     */
+    @Test
+    public void whenShouldCheckAdd() throws Exception {
+        list = new MyLinkedList<String>();
+        list.add("Test1");
+        list.add("Test2");
+        list.add("Test3");
+        list.add("Test4");
+        list.add("Test5");
+        int result = list.getSize();
+        assertThat(result, is(5));
+    }
+    /**
+     * This method allow to check method get.
+     * If get object by index equel choosed.
+     * @throws Exception
+     */
+    @Test
+    public void whenShouldCheckGetArr() throws Exception {
+        list = new MyLinkedList<String>();
+        list.add("Test1");
+        list.add("Test2");
+        list.add("Test3");
+        list.add("Test4");
+        list.add("Test5");
+        String result = list.getArr(2);
+        assertThat(result, is("Test3"));
+    }
 }
