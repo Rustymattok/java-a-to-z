bd5f4eeb4a11a366ebbed1058be028901dee02ff
Vladimir Makarov <mrArxi@gmail.com>
2016-11-06 12:52:44 +0300

    edited and add new Task for MyQueue.

---
+++ lesson5/src/main/java/part3list/MyQueue.java
@@ -0,0 +1,21 @@
+package part3list;
+/**
+ * Own class for queue.
+ * @param <T>
+ */
+public class MyQueue<T> extends MyLinkedList<T> implements IQureContainer<T>{
+    /**
+     * This method for add element in queue.
+     * @param object - elemnt which add to the list.
+     */
+    public void push(T object) {
+        super.add(object);
+    }
+    /**
+     * THis method for removing element from the list by turn - top.
+     * @return - element which was deleted.
+     */
+    public T peek() {
+        return super.remove(super.getSize() - 1);
+    }
+}
--- lesson5/src/test/java/part3list/MyQueueTest.java
+++ lesson5/src/test/java/part3list/MyQueueTest.java
@@ -4,16 +4,16 @@ import org.junit.Test;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 /**
- * Class for testing MyStack.
+ * Class for testing MyQueue.
  */
-public class MyStackTest {
+public class MyQueueTest {
     /**
      * This method allow to check add(T object).
      * @throws Exception
      */
     @Test
     public void whenShouldCheckStackAdd() throws Exception {
-        MyStack<String> list = new MyStack<String>();
+        MyQueue<String> list = new MyQueue<String>();
         list.add("Test1");
         list.add("Test2");
         list.add("Test3");
@@ -27,14 +27,14 @@ public class MyStackTest {
      * @throws Exception
      */
     @Test
-    public void whenShouldCheckStackPeek() throws Exception {
-        MyStack<String> list = new MyStack<String>();
+    public void whenShouldCheckQueuePeek() throws Exception {
+        MyQueue<String> list = new MyQueue<String>();
         list.add("Test1");
         list.add("Test2");
         list.add("Test3");
         list.add("Test4");
         list.add("Test5");
         String result = list.peek();
-        assertThat(result, is("Test1"));
+        assertThat(result, is("Test5"));
     }
 }
