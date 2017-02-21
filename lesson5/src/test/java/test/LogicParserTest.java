cfb2284140afa61e01bd7528ed18a99fadc20c26
Rustymattok <mrArxi@gmail.com>
2017-02-22 01:50:07 +0300

    fff

--- lesson5/src/test/java/test/LogicParserTest.java
+++ lesson5/src/test/java/test/LogicParserTest.java
@@ -1,6 +1,9 @@
 package test;
 
 import org.junit.Test;
+
+import java.util.HashMap;
+
 /**
  * Class for testing class.
  */
@@ -13,6 +16,9 @@ public class LogicParserTest {
         long totalTime = System.currentTimeMillis() - startTime;
         System.out.println(totalTime);
         System.out.println("Quantities of orders : = " + logicParser.getListNew().size());
+        for (HashMap.Entry<Integer, Order> entry : logicParser.getListNew().entrySet()) {
+            if(entry.getValue().getOperation().equals("SELL"))
+            System.out.println(entry.getValue().getPrice() + "      " + entry.getKey() );
+        }
     }
-
 }
