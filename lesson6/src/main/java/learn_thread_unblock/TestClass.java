51e33b6cac1bd8c4462c0c6928d85ce99efaca10
Rustymattok <mrArxi@gmail.com>
2017-10-29 18:51:47 +0300

    updated data

--- lesson6/src/main/java/learn_thread_unblock/TestClass.java
+++ lesson6/src/main/java/learn_thread_unblock/TestClass.java
@@ -19,31 +19,16 @@ public class TestClass {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                threadCollection.update(3,"ahahah");
-            }
-        }).start();
 
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                threadCollection.update(3,"ahahah1");
-            }
-        }).start();
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                threadCollection.update(3,"ahahah2");
-            }
-        }).start();
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                threadCollection.update(3,"ahahah3");
-            }
-        }).start();
+        for ( int i = 0; i < 30; i++) {
+            int y = i;
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    threadCollection.update(3,"ahahah" + y);
+                }
+            }).start();
+        }
 
         try {
             Thread.sleep(1000);
--- lesson6/src/main/java/learn_thread_unblock/ThreadCollection.java
+++ lesson6/src/main/java/learn_thread_unblock/ThreadCollection.java
@@ -1,5 +1,9 @@
 package learn_thread_unblock;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.function.BiFunction;
+
 /**
  * Class for ConcurrentHashMap. If we use this collection we don't need to add any Lock mechanizm and so on.
  */
@@ -7,25 +11,45 @@ public class ThreadCollection {
 
     private ConcurrentHashMap<Integer,Task> list = new ConcurrentHashMap<Integer,Task>();
     private int id =  0;
-   // Lock lock = new ReentrantLock();
+    Lock lock = new ReentrantLock();
     public void add(Task task) {
         id++;
         list.put(id,task);
     }
 
-        public void update(int id, String newName) {
-            int version = list.get(id).getVersion();
-            list.get(id).setName(newName);
-            System.out.println(version);
-            if (list.get(id).getVersion() == version ) {
+//    public void update(int id, String newName) {
+//            int version = list.get(id).getVersion();
+//            System.out.println(version);
+//            if (list.get(id).getVersion() == version ) {
+//                list.get(id).setName(newName);
+//                System.out.println("сколько раз");
+//            }else {
+//                try {
+//                    throw new OptimalException("Косяк");
+//                } catch (OptimalException e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//    }
+
+    public synchronized void update(int id, String newName){
+        int version = list.get(id).getVersion();
+        System.out.println(version);
+        if (list.get(id).getVersion() == version ) {
+            BiFunction<Integer,Task,Task> biFunction = (k, v) -> {
+                v.setName(newName);
+                return v;
+            };
+            list.computeIfPresent(id,biFunction);
             }else {
-                try {
-                    throw new OptimalException("Косяк");
-                } catch (OptimalException e) {
-                    e.printStackTrace();
-                }
+            try {
+                throw new OptimalException("Косяк");
+            } catch (OptimalException e) {
+                e.printStackTrace();
             }
+        }
     }
+
 
     public void show(){
         for ( ConcurrentHashMap.Entry<Integer,Task> data : list.entrySet()) {
