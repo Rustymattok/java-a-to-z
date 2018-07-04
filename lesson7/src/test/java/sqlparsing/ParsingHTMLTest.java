803e340f6b0d7b812b475faa15cc6115c8bcbf6e
Rustymattok <mrArxi@gmail.com>
2018-07-04 21:17:04 +0300

    add

--- lesson7/src/test/java/sqlparsing/ParsingHTMLTest.java
+++ lesson7/src/test/java/sqlparsing/ParsingHTMLTest.java
@@ -1,5 +1,15 @@
-import static org.junit.Assert.*;
-
+package sqlparsing;
+import org.junit.Test;
+/**
+ * Class for checking full application.
+ */
 public class ParsingHTMLTest {
-
+    public DBPropereties dbPropereties = new DBPropereties("/home/rustymattok/MyGit/lesson7/src/main/java/sqlparsing/files/app.properties");
+    @Test
+    public void whenShouldCheckParser() {
+        dbPropereties.initData();
+        DataBase dataBase = new DataBase(new WorkBase(dbPropereties.getUrl(), dbPropereties.getNameTable(), dbPropereties.getUsername(), dbPropereties.getPassword()));
+        ParsingHTML parsingHTML = new ParsingHTML(dbPropereties.getWeblink(), dataBase,dbPropereties.getFileWay());
+        parsingHTML.startParsing();
+    }
 }
