a7c08a2f00b3bdc9c0f68f35c6f109ad498e7118
Rustymattok <mrArxi@gmail.com>
2020-08-04 00:39:15 +0300

    - update functional interface to Predator

--- Lambda/src/main/java/ru/makarov/innerfunctionalinterface/SearchAtt.java
+++ Lambda/src/main/java/ru/makarov/innerfunctionalinterface/SearchAtt.java
@@ -5,35 +5,25 @@ import ru.makarov.anonimclass.Attachment;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.function.Function;
+import java.util.function.Predicate;
 
 public class SearchAtt {
+
     public static List<Attachment> fileSizeFunc(List<Attachment> list, int size) {
-        Function<Attachment, Attachment> function = attachment -> {
-            if (attachment.getSize() > size) {
-                return attachment;
-            }
-            return null;
-        };
+        Predicate<Attachment> function = attachment -> (attachment.getSize() > size);
         return filter(list, function);
     }
 
     public static List<Attachment> filterNameFunc(List<Attachment> list, String tag) {
-        Function<Attachment, Attachment> function = attachment -> {
-            if (attachment.getName().contains(tag)) {
-                return attachment;
-            }
-            return null;
-        };
+        Predicate<Attachment> function = attachment -> (attachment.getName().contains(tag));
         return filter(list, function);
     }
 
-    private static List<Attachment> filter(List<Attachment> list, Function<Attachment, Attachment> function) {
+    private static List<Attachment> filter(List<Attachment> list, Predicate<Attachment> function) {
         List<Attachment> rsl = new ArrayList<>();
         for (Attachment att : list) {
-            Attachment attachment = function.apply(att);
-            if (attachment != null) {
-                rsl.add(attachment);
+            if (function.test(att)) {
+                rsl.add(att);
             }
         }
         return rsl;
@@ -53,6 +43,7 @@ public class SearchAtt {
         attachmentsByName = filterNameFunc(attachments, "bug");
         System.out.println(attachmentsBySize);
         System.out.println(attachmentsByName);
+
 
     }
 }
