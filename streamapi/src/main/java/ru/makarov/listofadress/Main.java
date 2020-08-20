b57c6febb16e753fe3dc27a197c2077ea610c075
Rustymattok <mrArxi@gmail.com>
2020-08-20 15:33:08 +0300

    - update uniq sort

--- streamapi/src/main/java/ru/makarov/listofadress/Main.java
+++ streamapi/src/main/java/ru/makarov/listofadress/Main.java
@@ -1,9 +1,8 @@
 package ru.makarov.listofadress;
 
-import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
@@ -11,11 +10,14 @@ public class Main {
                 new Profile(new Address("Moscow", "Yb", 34, 101)),
                 new Profile(new Address("Mitishi", "Mb", 21, 231)),
                 new Profile(new Address("Dolgoprudniu", "Nb", 34, 78)),
-                new Profile(new Address("Korolev", "Sl", 75, 20)),
-                new Profile(new Address("Moscow", "Rb", 11, 63))
+                new Profile(new Address("Mitishi", "Sl", 75, 20)),
+                new Profile(new Address("Moscow", "Rb", 11, 63)),
+                new Profile(new Address("Moscvobad", "Rb", 11, 63))
         );
 
         List<Address> addressList = new Profiles().collect(profileList);
+        Comparator<Address> comparable = (o1, o2)-> -o1.getCity().compareTo(o2.getCity());
+        addressList.sort(comparable);
         addressList.stream().map(Address::getCity).distinct().forEach(System.out::println);
     }
 }
