7325f95a6ce66dd230e4274d9b55dcf2dd3d1e6d
Rustymattok <mrArxi@gmail.com>
2020-08-20 14:39:25 +0300

    - add update logic of app.

--- streamapi/src/main/java/ru/makarov/filterstudent/SchoolTest.java
+++ streamapi/src/main/java/ru/makarov/filterstudent/SchoolTest.java
@@ -9,17 +9,17 @@ public class SchoolTest {
                 new Student("Makarov", 100),
                 new Student("Ivanov1", 60),
                 new Student("Ivanov2", 80),
-                new Student("Ivanov3", 30),
+                new Student("Ivanov3", 50),
                 new Student("Ivanov4", 20),
-                new Student("Ivanov5", 90),
+                new Student("Ivanov5", 70),
                 new Student("Ivanov6", 10),
                 new Student("Ivanov7", 35)
         );
 
         School schoolA = new School();
         List<Student> groupA = schoolA.collect(group, student -> (student.getScore() >= 70 && student.getScore() <= 100));
-        List<Student> groupB = schoolA.collect(group, student -> (student.getScore() >= 50 && student.getScore() <= 70));
-        List<Student> groupC = schoolA.collect(group, student -> (student.getScore() >= 0 && student.getScore() <= 50));
+        List<Student> groupB = schoolA.collect(group, student -> (student.getScore() >= 50 && student.getScore() < 70));
+        List<Student> groupC = schoolA.collect(group, student -> (student.getScore() >= 0 && student.getScore() < 50));
 
         groupA.stream()
                 .map(student -> (student.getSurname() + " " + student.getScore() + " ;"))
