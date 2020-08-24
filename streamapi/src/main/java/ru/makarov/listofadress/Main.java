bd4f450fa01d7127fa33bf91d4d966804df3776e
Rustymattok <mrArxi@gmail.com>
2020-08-24 14:59:10 +0300

    - add updates due comments

--- streamapi/src/main/java/ru/makarov/listofadress/Main.java
+++ streamapi/src/main/java/ru/makarov/listofadress/Main.java
@@ -1,23 +1,20 @@
 package ru.makarov.listofadress;
 
-import java.util.Arrays;
-import java.util.Comparator;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        List<Profile> profileList = Arrays.asList(
-                new Profile(new Address("Moscow", "Yb", 34, 101)),
-                new Profile(new Address("Mitishi", "Mb", 21, 231)),
-                new Profile(new Address("Dolgoprudniu", "Nb", 34, 78)),
-                new Profile(new Address("Mitishi", "Sl", 75, 20)),
-                new Profile(new Address("Moscow", "Rb", 11, 63)),
-                new Profile(new Address("Moscvobad", "Rb", 11, 63))
-        );
+        List<Profile> profileList = new ArrayList<>();
+        profileList.add(new Profile(new Address("Moscow", "Yb", 34, 101)));
+        profileList.add(new Profile(new Address("Mitishi", "Mb", 21, 231)));
+        profileList.add(new Profile(new Address("Dolgoprudniu", "Nb", 34, 78)));
+        profileList.add(new Profile(new Address("Mitishi", "Sl", 75, 20)));
+        profileList.add(new Profile(new Address("Moscow", "Rb", 11, 63)));
+        profileList.add(new Profile(new Address("Moscvobad", "Rb", 11, 63)));
 
         List<Address> addressList = new Profiles().collect(profileList);
-        Comparator<Address> comparable = (o1, o2)-> -o1.getCity().compareTo(o2.getCity());
-        addressList.sort(comparable);
+
         addressList.stream().map(Address::getCity).distinct().forEach(System.out::println);
     }
 }
--- streamapi/src/main/java/ru/makarov/listofadress/Profiles.java
+++ streamapi/src/main/java/ru/makarov/listofadress/Profiles.java
@@ -1,10 +1,12 @@
 package ru.makarov.listofadress;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Profiles {
     List<Address> collect(List<Profile> profiles) {
-        return profiles.stream().map(Profile::getAddress).collect(Collectors.toList());
+        Comparator<Profile> comparable = (o1, o2)-> -o1.getAddress().getCity().compareTo(o2.getAddress().getCity());
+        return profiles.stream().sorted(comparable).map(Profile::getAddress).collect(Collectors.toList());
     }
 }
