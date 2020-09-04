7325c83cbd66ae46e98456648099826750b975e6
Rustymattok <mrArxi@gmail.com>
2020-09-04 16:14:27 +0300

    - add Hashmap

--- lesson5/src/main/java/texttext/Article.java
+++ lesson5/src/main/java/texttext/Article.java
@@ -1,14 +1,29 @@
 package texttext;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Article {
-    public static boolean generateBy(String origin, String line) {
-        String[] lineData = line.toLowerCase().split("\\s*(\\s|,|!|\\.)\\s*");
+    public static boolean generateBy(String origin, String line){
         boolean flag = true;
-        for (int i = 0; i < lineData.length; i++) {
-            if(!origin.contains(lineData[i])){
-                flag =false;
+        String[] lineData = line.toLowerCase().split("\\s*(\\s|,|!|\\.)\\s*");
+        String[] originData = origin.toLowerCase().split("\\s*(\\s|,|!|\\.)\\s*");
+        Map<String,Integer> mapOrigin = new HashMap<>();
+
+        for (int i = 0; i < originData.length; i++) {
+            if(mapOrigin.get(originData[i]) != null) {
+                mapOrigin.put(originData[i], mapOrigin.get(originData[i]) + 1);
+            }else {
+                mapOrigin.put(originData[i],0);
             }
         }
+
+        for (int i = 0; i < lineData.length; i++) {
+            if(mapOrigin.get(lineData[i]) == null){
+                flag = false;
+            }
+        }
+
         return flag;
     }
 }
