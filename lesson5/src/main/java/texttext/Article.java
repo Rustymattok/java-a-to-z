cb3f653a67f6ca09689a014e7aa2fbfc77a5abdc
Rustymattok <mrArxi@gmail.com>
2020-09-04 10:50:07 +0300

    - more simple method for compare

--- lesson5/src/main/java/texttext/Article.java
+++ lesson5/src/main/java/texttext/Article.java
@@ -1,22 +1,14 @@
 package texttext;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.TreeSet;
-import java.util.stream.Collectors;
-
 public class Article {
     public static boolean generateBy(String origin, String line) {
-        List<String> listOrigin = Arrays.asList(origin.split("\\s*(\\s|,|!|\\.)\\s*"));
-        List<String> listLine = Arrays.asList(line.split("\\s*(\\s|,|!|\\.)\\s*"));
-        listOrigin = listOrigin.stream()
-                .map(String::toLowerCase)
-                .collect(Collectors.toList());
-        listLine =  listLine.stream()
-                .map(String::toLowerCase)
-                .collect(Collectors.toList());
-        TreeSet<String> treeOrigin = new TreeSet<>(listOrigin);
-        TreeSet<String> treeLine = new TreeSet<>(listLine);
-        return treeOrigin.containsAll(treeLine);
+        String[] lineData = line.toLowerCase().split("\\s*(\\s|,|!|\\.)\\s*");
+        boolean flag = true;
+        for (int i = 0; i < lineData.length; i++) {
+            if(!origin.contains(lineData[i])){
+                flag =false;
+            }
+        }
+        return flag;
     }
 }
